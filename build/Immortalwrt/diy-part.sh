78568b2f4744e0b0ceae12aa7d8ce68610717fea
qiang1352 <79758189+qiang1352@users.noreply.github.com>
2024-10-03 12:15:57 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -4,8 +4,6 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
 # 后台IP设置
 export Ipv4_ipaddr="192.168.5.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
